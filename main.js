e3085508f3e682785961f005a7f6ed8ac1ba0c18
Mrinmoy Nath <86364520+Mrin45@users.noreply.github.com>
2023-09-28 18:43:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,15 +13,6 @@ var loader_js = $.ajax({
 	}
 })
 
-//loading vill overlay file 
-var loaderV_js = $.ajax({
-	url: "getVillOverlay.js",
-	dataType: "script",
-	success: console.log("vill overlay filter module loaded"),
-	error: function (xhr) {
-		alert(xhr.statusText)
-	}
-})
 
 
 $.when(loader_js, loaderV_js).done(function () {
@@ -74,12 +65,7 @@ $.when(loader_js, loaderV_js).done(function () {
 			//call function,load the map
 			mapLoadnReset();
 
-			//list of an id, mapped and extracted unique keys based on identifier
-			var allGroup = d3.map(data, function (d) { return (d.District) }).keys();
-			var allTypes = d3.map(data, function (d) { return (d.Village) }).keys();
-			var allGrad = d3.map(data, function (d) { return (d.Garden_Nam) }).keys();
-			var allFact = d3.map(data, function (d) { return (d.name) }).keys();
-			//console.log(allGroup);
+			
 
 			//visualization update of markers on the basis of id selected
 			function updateViz(selectedDistrict) {
@@ -90,30 +76,7 @@ $.when(loader_js, loaderV_js).done(function () {
 				//update total data count in the selected district
 				$("#dataCount").html(myFilteredData.length)
 
-				//clear markers within previous marker group
-				myMarkerGroup.clearLayers()
-
-				//overlay the dist layer
-				//Add requested external GeoJSON to map
-				distOverlayurl = getOverlay(selectedDistrict)
-
-				if (distOverlaydata != undefined) { map.removeLayer(distOverlaydata); }
-
-				$.getJSON(distOverlayurl, function (data) {
-
-					distOverlaydata = L.geoJSON(data).addTo(map);
-					//console.log(data)
-
-				});
-
-				//get coordinates for selectedDistrict
-				coord = getCoord(selectedDistrict)
-
-				//fly to region of interest
-				map.flyTo([coord[0], coord[1]], 10, {
-					animate: true,
-					duration: 2 // in seconds
-				});
+	
 
 				//plot new data derived from filter function
 				myFilteredData.forEach(function (d) {
@@ -138,24 +101,6 @@ $.when(loader_js, loaderV_js).done(function () {
 					marker3.closeTooltip();
 				});
 
-				myFilteredData.forEach(function (d) {
-					marker3 = L.marker([d.latitude_y, d.longitude_x], {
-						icon: L.icon({
-							iconUrl: "assets/factTEA1.png",
-							iconSize: [32, 32], // Adjust the size as per your requirement
-							iconAnchor: [16, 32], // Adjust the anchor point if needed
-						}),
-					}).bindTooltip(d.name).bindPopup(
-						'<div style="font-size: 13px;">' +
-						'<strong>Factory Name:</strong> ' + d.name +
-						'</div>'
-					).addTo(myMarkerGroup);
-
-					marker3.openTooltip();
-					marker3.closeTooltip();
-				});
-
-			}
 
 
 
@@ -246,13 +191,7 @@ $.when(loader_js, loaderV_js).done(function () {
 				.text(function (d) { return d; }) //text under dropdown
 				.attr("value", function (d) { return d; })
 
-			//update on dropdown select event
-			d3.select("#selectButton1").on("change", function (d) {
-				selectedVillage = this.value
-				//console.log(selectedVillage)
-				updateVizz(selectedVillage)
-			})
-
+	
 			/*dropdown for tea garden filter */
 			//adding options to selectButton2
 			d3.select("#selectButton2")
@@ -275,27 +214,7 @@ $.when(loader_js, loaderV_js).done(function () {
 					duration: 2 // in seconds
 				});
 				myFilteredData.forEach(function (d) {
-					marker3 = L.marker([d.Latitude, d.Longitude], { icon: getIcon(d.Village) }).bindTooltip(d.Garden_Nam).bindPopup(
-						'<div style="font-size: 13px;">' +
-						'<strong>Garden Name:</strong> ' + d.Garden_Nam +
-						'<br/><strong>Grower Name:</strong> ' + d.Grower_Nam +
-						'<br/><strong>District:</strong> ' + d.District +
-						'<br/><strong>Subdivision:</strong> ' + d.Subdivision +
-						'<br/><strong>ID No.:</strong> ' + d.Identification +
-						'<br/><strong>Total Tea Garden Area (in bigha):</strong> ' + d.Total_Tea +
-						'<br/><strong>Village:</strong> ' + d.Village +
-						'<br/><strong>Community:</strong> ' + d.Community +
-						'<br/><strong>Land Category:</strong> ' + d.Land_Categ +
-						'<br/><strong>Nearby Factory:</strong> ' + d.Nearby_fac +
-						'<br/><strong>Total Production of Tea Leaf / Year (in kg):</strong> ' + d.Total_prod +
-						'<br/><strong>No. of Registered Plots:</strong> ' + d.Number_of +
-						'</div>'
-					).addTo(myMarkerGroup);
-
-					marker3.openTooltip();
-					marker3.closeTooltip();
-				});
-
+				
 			});
 
 			/*dropdown for tea factory filter */
@@ -373,44 +292,11 @@ $.when(loader_js, loaderV_js).done(function () {
 
 
 		// // Instantiate sidebar, open when Disclaimer modal is closed
-		// var sidebar = L.control.sidebar('sidebar', {
-		// 	position: 'left'
-		// }).addTo(map);
-
-		// $('#disclaimer-modal').on('hidden.bs.modal', function () {
-		// 	sidebar.show();
-		// });
-
-		// Add native looking Leaflet buttons with Font Awesome icons
-		// L.easyButton(
-		// 	'fa-question-circle',
-		// 	function () {
-		// 		$('#disclaimer-modal').modal();
-		// 		sidebar.hide();
-		// 	},
-		// 	'Help!',
-		// 	map
-		// );
-
-		// L.easyButton(
-		// 	'fa-list',
-		// 	function () { sidebar.toggle(); },
-		// 	'Legend Information',
-		// 	map
-		// );
-
+		// var
 		L.control.scale().addTo(map);
 		L.control.layers(basemaps, overlays).addTo(map);
 		// // Measure control
-		// var measure = L.control.measure({
-		// 	primaryLengthUnit: 'feet',
-		// 	secondaryLengthUnit: 'miles',
-		// 	primaryAreaUnit: 'acres',
-		// 	secondaryAreaUnit: 'square miles'
-		// });
-
-		// // Add the modified measurement control to the map
-		// measure.addTo(map);
+	
 
 	};
 
